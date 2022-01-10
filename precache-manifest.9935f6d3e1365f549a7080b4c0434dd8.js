2f9d115a706db305415d5150074c2d892f0978be
doodlenft <58586770+doodlenft@users.noreply.github.com>
2022-01-10 22:26:50 +0800

    Add files via upload

---
+++ precache-manifest.9935f6d3e1365f549a7080b4c0434dd8.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "af960fb2a7f5ed6fabb1",
+    "url": "/css/app.530bdc3d.css"
+  },
+  {
+    "revision": "af960fb2a7f5ed6fabb1",
+    "url": "/js/app.8d091fe6.js"
+  },
+  {
+    "revision": "e31563ce56bfe53be07f",
+    "url": "/css/chunk-vendors.ec54bd74.css"
+  },
+  {
+    "revision": "e31563ce56bfe53be07f",
+    "url": "/js/chunk-vendors.be0c0f19.js"
+  },
+  {
+    "revision": "e2660cc13ea75f84b21cba7152cd6104",
+    "url": "/img/2.e2660cc1.png"
+  },
+  {
+    "revision": "72d8bd83ef8f81df3e8ad80a8b1735b6",
+    "url": "/img/soldout.72d8bd83.png"
+  },
+  {
+    "revision": "12bf7e14061e7cf7ecf044136b217e2f",
+    "url": "/img/mint.12bf7e14.png"
+  },
+  {
+    "revision": "bc50459ead15bc7e2dd95a3c0bfeff04",
+    "url": "/img/dood.bc50459e.png"
+  },
+  {
+    "revision": "20841c2d288fadd8be670c6189208555",
+    "url": "/img/house.20841c2d.png"
+  },
+  {
+    "revision": "7803a75d4efad12834d8e8c0ae447cf2",
+    "url": "/img/1.7803a75d.png"
+  },
+  {
+    "revision": "145dc557845548a36a82337912ca3ac5",
+    "url": "/img/discord-white.145dc557.svg"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "cadaa137085c890eb8a320dd28660ab4",
+    "url": "/img/discord.cadaa137.svg"
+  },
+  {
+    "revision": "833629ad762db82d28267a08fec7e9a9",
+    "url": "/img/bg.833629ad.jpeg"
+  },
+  {
+    "revision": "300e3dd05ce96ef821a71bb9ba8f43f9",
+    "url": "/img/4.300e3dd0.png"
+  },
+  {
+    "revision": "fb08dd7a2ba24fe54c0d2078375a5467",
+    "url": "/img/twitter-white.fb08dd7a.svg"
+  },
+  {
+    "revision": "a432d37017e8e0bfe7eeb15f9da41071",
+    "url": "/img/twitter.a432d370.svg"
+  },
+  {
+    "revision": "fb8e321a14288d14018ae43b0627346e",
+    "url": "/img/3.fb8e321a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a53930b28dba29ca6739f0c105e5b88f",
+    "url": "/img/center.a53930b2.png"
+  },
+  {
+    "revision": "a986f1fc0f29ff7062b306cc9eb74487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  }
+];
